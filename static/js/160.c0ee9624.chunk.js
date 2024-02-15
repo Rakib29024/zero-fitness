3ab650d7cea4d0bdb0bdd52d4354563fb7a0531f
Md. Abdullah Al Rakib Hassan <abdullah.rakib@sslwireless.com>
2024-02-15 12:33:23 +0600

    Updates

---
+++ static/js/160.c0ee9624.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_free_react_admin_template=self.webpackChunk_coreui_coreui_free_react_admin_template||[]).push([[160],{9160:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=160.c0ee9624.chunk.js.map
